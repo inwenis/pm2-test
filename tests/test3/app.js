8672789dc7efea10bdf12700458684156c11ad20
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-02-16 22:07:17 +0100

    restart after being disconnected

--- tests/test3/app.js
+++ tests/test3/app.js
@@ -4,24 +4,30 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-const ws = new WebSocket('wss://echo.websocket.org/');
+function connect() {
+  const ws = new WebSocket('wss://echo.websocket.org/');
 
-ws.on('open', function open() {
-  console.log('Connected to the WebSocket server');
-  setInterval(() => {
-    const randomString = Math.random().toString(36).repeat(getRandomInt(100));
-    ws.send(randomString);
-  }, 10);
-});
+  ws.on('open', function open() {
+    console.log('Connected to the WebSocket server');
+    setInterval(() => {
+      const randomString = Math.random().toString(36).repeat(getRandomInt(100));
+      ws.send(randomString);
+    }, 50);
+  });
 
-ws.on('message', function incoming(data) {
-  console.log('Received data:', data);
-});
+  ws.on('message', function incoming(data) {
+    console.log('Received data:', data);
+  });
 
-ws.on('error', function error(err) {
-  console.error('WebSocket error:', err);
-});
+  ws.on('error', function error(err) {
+    console.error('WebSocket error:', err);
+  });
 
-ws.on('close', function close() {
-  console.log('Disconnected from the WebSocket server');
-});
+  ws.on('close', function close() {
+    console.log('Disconnected from the WebSocket server');
+    setTimeout(connect, 2000);
+  });
+
+}
+
+connect();
