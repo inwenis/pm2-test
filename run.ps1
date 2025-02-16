34fa9cacdfa9b77abfc902af22a7ae12b0891bd9
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-02-16 18:59:45 +0100

    run test and check memory periodically

--- run.ps1
+++ run.ps1
@@ -1,1 +1,16 @@
-docker run --memory=1024m pm2test:latest
+function Get-MemoryUsage {
+    while ($true) {
+        $now = Get-Date -Format "O"
+        $stats = docker stats --no-stream --format '{{.MemUsage}}' # sample output: 48.51MiB / 1GiB
+        $match = [Regex]::Match($stats, '([\d\.]+)(\w+) / ([\d\.]+)(\w+)')
+        $memUsage = [double]$match.Groups[1].Value
+        $memTotal = [double]$match.Groups[3].Value
+        $memUsageUnit = $match.Groups[2].Value
+        $memTotalUnit = $match.Groups[4].Value
+        Write-Output "$now,$memUsage,$memUsageUnit,$memTotal,$memTotalUnit"
+        Start-Sleep -Seconds 10
+    }
+}
+
+docker run --memory=512m --detach pm2test:latest
+Get-MemoryUsage | Tee-Object "./memory.csv"
