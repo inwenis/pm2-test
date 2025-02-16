96ded7720bf604b176651b7c16f68f09a96626b3
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-02-16 21:30:57 +0100

    make test 2 send requests

--- tests/test2/app.js
+++ tests/test2/app.js
@@ -1,6 +1,36 @@
-let counter = 0;
+const axios = require('axios');
 
-setInterval(() => {
-  const n = new Date().toISOString();
-  console.log(`[${n}] test1.js - running - counter: ${counter++}`);
+setInterval(async () => {
+  const urls = [
+    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/comments',
+    'https://jsonplaceholder.typicode.com/albums',
+    'https://jsonplaceholder.typicode.com/photos',
+    'https://jsonplaceholder.typicode.com/todos'
+  ];
+
+  for (const url of urls) {
+    try {
+      const r = await axios.get(url);
+      const n = new Date().toISOString();
+      console.log(`[${n}] received ${r.data.length} elements from ${url}`);
+    } catch (e) {
+      console.error(`Error sending GET to ${url}`, e);
+    }
+  }
+
+  const postUrl = 'https://jsonplaceholder.typicode.com/posts';
+  try {
+    const postData = {
+      title: 'foo',
+      body: 'bar',
+      userId: 1
+    };
+    const postResponse = await axios.post(postUrl, postData);
+    const n = new Date().toISOString();
+    console.log(`[${n}] send POST to ${postUrl} received ${postResponse.status} response`);
+  } catch (e) {
+    console.error(`Error sending POST to ${postUrl}:`, e);
+  }
+
 }, 10);
