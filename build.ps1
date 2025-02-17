b17a8d4d00d2470863a5712a540fa67adf2fdbd7
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-02-17 23:12:12 +0100

    tag with commit sha

--- build.ps1
+++ build.ps1
@@ -1,9 +1,10 @@
+$commitShort = git rev-parse --short HEAD
 $files = Get-ChildItem tests -Recurse -File
 $files | Where-Object Name -eq "Dockerfile" | ForEach-Object {
     $dir = $_.DirectoryName
     Push-Location $dir
     $content = Get-Content $_
-    $command = $content[0].Replace("# ", "")
+    $command = $content[0].Replace("# ", "").Replace("{{short}}", $commitShort)
     Write-Host "Running $command in $dir"
     Invoke-Expression $command
     Pop-Location
