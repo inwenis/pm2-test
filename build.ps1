47be2641f3bcf556ef637171090f00469dc3f07e
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-02-18 23:34:56 +0100

    set strict mode and fix errors

--- build.ps1
+++ build.ps1
@@ -1,12 +1,14 @@
+Set-StrictMode -Version Latest
+
 $commitShort = git rev-parse --short HEAD
 $files = Get-ChildItem tests -Recurse -File | Where-Object Name -eq "Dockerfile"
 foreach ($file in $files) {
     Write-Host "Will build: $($file.FullName)"
 }
 foreach ($file in $files) {
-    $dir = $_.DirectoryName
+    $dir = $file.DirectoryName
     Push-Location $dir
-    $content = Get-Content $_
+    $content = Get-Content $file
     $command = $content[0].Replace("# ", "").Replace("{{short}}", $commitShort)
     Write-Host "Running $command in $dir"
     Invoke-Expression $command
--- run.ps1
+++ run.ps1
@@ -1,3 +1,5 @@
+Set-StrictMode -Version Latest
+
 function Get-MemoryUsage() {
     while ($true) {
         $containers = docker ps --format '{{.ID}},{{.Image}}'
