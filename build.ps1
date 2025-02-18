3e7e452b36f78b720467e71276da05cd0fca9d7f
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-02-18 23:12:52 +0100

    refactor - print all before building and use foreach loop

--- build.ps1
+++ build.ps1
@@ -1,6 +1,9 @@
 $commitShort = git rev-parse --short HEAD
-$files = Get-ChildItem tests -Recurse -File
-$files | Where-Object Name -eq "Dockerfile" | ForEach-Object {
+$files = Get-ChildItem tests -Recurse -File | Where-Object Name -eq "Dockerfile"
+foreach ($file in $files) {
+    Write-Host "Will build: $($file.FullName)"
+}
+foreach ($file in $files) {
     $dir = $_.DirectoryName
     Push-Location $dir
     $content = Get-Content $_
